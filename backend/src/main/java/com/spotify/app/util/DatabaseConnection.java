2827b2503255d23826aea22c7ea61d0ec4a864b4
Janis Gruettmueller <janis.gr@outlook.com>
2024-09-17 14:14:56 +0200

    bug fixing db connection

--- backend/src/main/java/com/spotify/app/util/DatabaseConnection.java
+++ backend/src/main/java/com/spotify/app/util/DatabaseConnection.java
@@ -1,31 +1,20 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv; use when db data sensitive
 
 public class DatabaseConnection {
-    private static final Dotenv dotenv = Dotenv.configure().directory("/Users/janisgruettmueller/web-app-root/config/").filename("database.env").load();
-    private static final String JDBC_URL = "jdbc:mysql://" + dotenv.get("DB_HOST") + ":" + dotenv.get("DB_PORT") + "/" + dotenv.get("DB_NAME");
-    private static final String USER = dotenv.get("DB_USER");
-    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
+    // private static final Dotenv dotenv = Dotenv.configure().directory("/Users/janisgruettmueller/web-app-root/config/").filename("database.env").load();
+    // private static final String JDBC_URL = "jdbc:mysql://" + dotenv.get("DB_HOST") + ":" + dotenv.get("DB_PORT") + "/" + dotenv.get("DB_NAME");
+    // private static final String USER = dotenv.get("DB_USER");
+    // private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
-    // This method provides the database connection
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/spotify";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "";
+
+    // Method to connect to database
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
-    }
-
-    public static void main(String[] args) {
-        try {
-            Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
-            if (connection != null) {
-                System.out.println("Connected to the database successfully!");
-            } else {
-                System.out.println("Failed to connect to the database.");
-            }
-            // Close the connection
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }  
+        return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
     }
 }
